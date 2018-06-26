88c62230514d10f4310b00ed5916365b84ab5db3
jgphillips77 <jgphillips77@gmail.com>
2018-06-25 20:44:26 -0600

    update
    June update

--- __init__.py
+++ __init__.py
@@ -7,10 +7,11 @@ from scipy import stats
 from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-from sklearn.model_selection import train_test_split, GridSearchCV, ShuffleSplit
-from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV, ShuffleSplit, cross_val_score
+from sklearn.linear_model import LogisticRegression, LinearRegression, Lasso, Ridge, ElasticNet
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score, cross_val_predict
 
 from sklearn.feature_selection import SelectFromModel, SelectKBest, RFE, f_regression, chi2, f_classif
 from sklearn import svm
@@ -26,4 +27,5 @@ from sklearn.ensemble import AdaBoostClassifier, AdaBoostRegressor
 
 from sklearn.cluster import KMeans, k_means
 from sklearn import metrics
-from sklearn.metrics import silhouette_score, confusion_matrix, accuracy_score
+from sklearn.metrics import silhouette_score, confusion_matrix, accuracy_score, classification_report, roc_auc_score, roc_curve
+from sklearn.metrics import mean_squared_error
